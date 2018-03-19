6c844d190879faee0e1853b74e3a4ddae89869d2
Thales Francisco <arquimago@users.noreply.github.com>
2018-03-19 09:08:59 -0300

    Create androidse.py

---
+++ androidse.py
@@ -0,0 +1,38 @@
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater, Job
+
+token = 'O TOKEN AQUI'
+
+
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+
+def start(bot, update):	  
+	bot.sendMessage(chat_id=update.message.chat_id, text="Isto fica feliz em ser útil! \n Estou ajudando o canal @androidse a crescer!!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+def welcome(bot, update):
+	if(update.message.new_chat_member):
+		chat_id = update.message.chat.id
+		new_user = ""
+				
+		if(update.message.new_chat_member.username != ""):
+			new_user = "@" + update.message.new_chat_member.username;
+		else:
+			new_user = update.message.new_chat_member.first_name;
+
+		bemvindo = "Seja muito bem vindo " + new_user + "!!!" + "\n" + "Aproveite o espaço, tire suas duvidas e ajude o crescimento da comunidade!\n Não se esqueça de se inscrever no https://www.meetup.com/pt-BR/android-sergipe/ !"
+		
+		bot.sendMessage(chat_id=chat_id, text=bemvindo)
+
+welcome_handler = MessageHandler(Filters.status_update, welcome)
+dispatcher.add_handler(welcome_handler)
+
+def git(bot, update):
+	bot.sendMessage(chat_id=update.message.chat_id, text="O código deste bot se encontra em http://github.com/arquimago/androidse sinta-se a vontade para fazer seu pull request!")
+
+git_handler = CommandHandler('git', git)
+dispatcher.add_handler(git_handler)
+
+updater.start_polling()
